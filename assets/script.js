d31da97ed33ec328fa33fa074cd500da562ad4b9
Alex Kibler <alexkibler@me.com>
2017-10-06 14:02:08 -0400

    Add basic keystroke addition and background image

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,9 @@
-console.log('script loaded successfully')
+
+$(function(){
+  $(window).keydown(function(e) {
+    if (e.which === 65 || e.which === 90) {
+      console.log('go up');
+      $(".bar").height('+=30');
+    }
+  })
+})
