98b2cffb115d725156c32c60a3dab25f76da470e
Alex Kibler <alexkibler@me.com>
2017-10-06 15:02:25 -0400

    Basic syle mockup

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,24 @@
 
 $(function(){
   $(window).keydown(function(e) {
+    console.log(e.which);
     if (e.which === 65 || e.which === 90) {
-      console.log('go up');
-      $(".bar").height('+=30');
+      $('#bar-player-1').stop(true, false);
+      $('#bar-player-1').height('+=50');
+      $('#bar-player-1').animate({height: 0}, 200);
+    }
+    if (e.which === 190 || e.which === 191) {
+      $('#bar-player-2').stop(true, false);
+      $('#bar-player-2').height('+=50');
+      $('#bar-player-2').animate({height: 0}, 200);
     }
   })
 })
+
+function Player(Number) {
+  this.number = number;
+  this.score = 0;
+  this.addScore = function(score) {
+    return this.score += score;
+  }
+}
