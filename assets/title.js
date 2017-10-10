667631fedb298e2dce7ea32b519655d729e1c219
Alex Kibler <alexkibler@me.com>
2017-10-10 17:23:34 -0400

    Ensure new games started from index.html start at level 1

--- assets/title.js
+++ assets/title.js
@@ -2,8 +2,20 @@ $(function (){
   console.log('page loaded')
   $('#play-link').click(function(){
     $('body').append('<div id="shade" class="fade"></div>')
+    localStorage.setItem('level',1);
+    localStorage.setItem('1-score',0);
+    localStorage.setItem('2-score',0);
     setTimeout(function(){
       window.location.href = 'game.html';
     }, 1500);
+  })
+  $('#instructions-link').click(function(){
+    $('body').append('<div id="instruction" ></div>')
+    $('#instruction').html('<p>Test Paragraph</p>');
+  })
+  $(window).keydown(function(e) {
+    if (e.which === 27) {
+      $('#instruction').remove();
+    }
   })
 })
